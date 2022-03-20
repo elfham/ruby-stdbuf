1a19f6f9c01ab39fd7d885e722ae861917a97edf
elfham <38372058+elfham@users.noreply.github.com>
2022-03-20 13:23:08 +0900

    refactor

--- README.md
+++ README.md
@@ -11,13 +11,13 @@ Add `require 'stdbuf'` into your code.
 Then use stdbuf(1).
 
 ```
-% ruby -I. -rstdbuf -e'loop{puts Time.now; sleep 1}' | cat
+% ruby -rstdbuf -e'loop{puts Time.now; sleep 1}' | cat
 ^C-e:1:in `sleep': Interrupt
         from -e:1:in `block in <main>'
         from -e:1:in `loop'
         from -e:1:in `<main>'
 
-% stdbuf -o 0 ruby -I. -rstdbuf -e'loop{puts Time.now; sleep 1}' | cat
+% stdbuf -o 0 ruby -rstdbuf -e'loop{puts Time.now; sleep 1}' | cat
 2018-02-23 12:43:05 +0900
 2018-02-23 12:43:06 +0900
 2018-02-23 12:43:07 +0900
--- lib/stdbuf.rb
+++ lib/stdbuf.rb
@@ -1,41 +1,47 @@
-def stdbuf(env = ENV)
-  case RUBY_PLATFORM
-  when /netbsd/i
-    stdbuf_all = env['STDBUF']
-    {
-      'STDBUF0' => STDIN,
-      'STDBUF1' => STDOUT,
-      'STDBUF2' => STDERR,
-    }.each do |key, io|
-      value = env[key] || stdbuf_all
-      next unless value
+# stdbuf support
+module Stdbuf
+  VERSION = '0.0.0'
 
-      case value
-      when 'U', 'u', 'L', 'l', '0'
-        io.sync = true
-      when 'F', 'f', /\A\d+\z/
-        io.sync = false
+  def self.stdbuf(env = ENV)
+    case RUBY_PLATFORM
+    when /netbsd/i
+      stdbuf_all = env['STDBUF']
+      {
+        'STDBUF0' => STDIN,
+        'STDBUF1' => STDOUT,
+        'STDBUF2' => STDERR,
+      }.each do |key, io|
+        value = env[key] || stdbuf_all
+        next unless value
+
+        case value
+        when 'U', 'u', 'L', 'l', '0'
+          io.sync = true
+        when 'F', 'f', /\A\d+\z/
+          io.sync = false
+        end
       end
-    end
-  else  # Linux (GNU coreutils), FreeBSD, etc...
-    return if !env.key?('LD_PRELOAD') || /\blibstdbuf.so\b/ !~ env['LD_PRELOAD']
+    else  # Linux (GNU coreutils), FreeBSD, etc...
+      return unless env.key?('LD_PRELOAD')
+      return if /\blibstdbuf.so\b/ !~ env['LD_PRELOAD']
 
-    {
-      '_STDBUF_I' => STDIN,
-      '_STDBUF_O' => STDOUT,
-      '_STDBUF_E' => STDERR,
-    }.each do |key, io|
-      value = env[key]
-      next unless value
+      {
+        '_STDBUF_I' => STDIN,
+        '_STDBUF_O' => STDOUT,
+        '_STDBUF_E' => STDERR,
+      }.each do |key, io|
+        value = env[key]
+        next unless value
 
-      case value
-      when '0', 'L'
-        io.sync = true
-      when 'B', /\A\d+(?:[kKMGTPEZY]B?)?\z/
-        io.sync = false
+        case value
+        when '0', 'L'
+          io.sync = true
+        when 'B', /\A\d+(?:[kKMGTPEZY]B?)?\z/
+          io.sync = false
+        end
       end
     end
   end
 end
 
-stdbuf(ENV)
+Stdbuf.stdbuf(ENV)
